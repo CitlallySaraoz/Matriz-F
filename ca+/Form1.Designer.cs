646f6443bcc9d9ee0261d5979f2acf21b63b2b67
citlallysaraoz <delacruzsaraozkarlacitlally@gmail.com>
2021-12-11 11:15:05 -0600

    first commit

---
+++ ca+/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace ca_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtColumnas = new System.Windows.Forms.TextBox();
+            this.txtFilas = new System.Windows.Forms.TextBox();
+            this.dgvMA = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvMB = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgvResultado = new System.Windows.Forms.DataGridView();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.btnRestar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(409, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora de matricez";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(116, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Num filas:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(62, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Num columnas:";
+            // 
+            // txtColumnas
+            // 
+            this.txtColumnas.Location = new System.Drawing.Point(256, 150);
+            this.txtColumnas.Name = "txtColumnas";
+            this.txtColumnas.Size = new System.Drawing.Size(48, 22);
+            this.txtColumnas.TabIndex = 3;
+            // 
+            // txtFilas
+            // 
+            this.txtFilas.Location = new System.Drawing.Point(256, 105);
+            this.txtFilas.Name = "txtFilas";
+            this.txtFilas.Size = new System.Drawing.Size(48, 22);
+            this.txtFilas.TabIndex = 4;
+            // 
+            // dgvMA
+            // 
+            this.dgvMA.AllowUserToAddRows = false;
+            this.dgvMA.AllowUserToDeleteRows = false;
+            this.dgvMA.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvMA.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvMA.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMA.Location = new System.Drawing.Point(66, 322);
+            this.dgvMA.Name = "dgvMA";
+            this.dgvMA.RowHeadersWidth = 51;
+            this.dgvMA.RowTemplate.Height = 24;
+            this.dgvMA.Size = new System.Drawing.Size(238, 203);
+            this.dgvMA.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(62, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Matriz A:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(360, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Matriz B:";
+            // 
+            // dgvMB
+            // 
+            this.dgvMB.AllowUserToAddRows = false;
+            this.dgvMB.AllowUserToDeleteRows = false;
+            this.dgvMB.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader;
+            this.dgvMB.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvMB.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMB.Location = new System.Drawing.Point(364, 322);
+            this.dgvMB.Name = "dgvMB";
+            this.dgvMB.RowHeadersWidth = 51;
+            this.dgvMB.RowTemplate.Height = 24;
+            this.dgvMB.Size = new System.Drawing.Size(238, 203);
+            this.dgvMB.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(692, 278);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Resultado:";
+            // 
+            // dgvResultado
+            // 
+            this.dgvResultado.AllowUserToAddRows = false;
+            this.dgvResultado.AllowUserToDeleteRows = false;
+            this.dgvResultado.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvResultado.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultado.Location = new System.Drawing.Point(696, 322);
+            this.dgvResultado.Name = "dgvResultado";
+            this.dgvResultado.RowHeadersWidth = 51;
+            this.dgvResultado.RowTemplate.Height = 24;
+            this.dgvResultado.Size = new System.Drawing.Size(238, 203);
+            this.dgvResultado.TabIndex = 9;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCargar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargar.Location = new System.Drawing.Point(479, 88);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(137, 39);
+            this.btnCargar.TabIndex = 11;
+            this.btnCargar.Text = "Continuar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSumar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSumar.Location = new System.Drawing.Point(479, 148);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(137, 39);
+            this.btnSumar.TabIndex = 12;
+            this.btnSumar.Text = "Sumar";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestar.Location = new System.Drawing.Point(636, 88);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(137, 39);
+            this.btnRestar.TabIndex = 13;
+            this.btnRestar.Text = "Restar";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            this.btnRestar.Click += new System.EventHandler(this.btnRestar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 567);
+            this.Controls.Add(this.btnRestar);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dgvResultado);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dgvMB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dgvMA);
+            this.Controls.Add(this.txtFilas);
+            this.Controls.Add(this.txtColumnas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtColumnas;
+        private System.Windows.Forms.TextBox txtFilas;
+        private System.Windows.Forms.DataGridView dgvMA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvMB;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgvResultado;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Button btnRestar;
+    }
+}
+
---
+++ ca+/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace ca_
+{
+    public partial class Form1 : Form
+    {
+
+        int[,] MatrizA = new int[100,100];
+        int[,] MatrizB = new int[100, 100];
+        int[,] MatrizResult = new int[100, 100];
+        int[,] MatrizSuma = new int[100, 100];
+        
+
+        int i, j, Filas, Columnas;
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+
+            Insertar();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            Sumar();
+        }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            Resar();+
+        }
+
+        private void Insertar()
+        {
+            Filas = Convert.ToInt32(txtFilas.Text);
+            Columnas = Convert.ToInt32(txtColumnas.Text);
+
+
+
+
+            dgvMA.RowCount = Filas;
+            dgvMA.ColumnCount = Columnas;
+
+            dgvMB.RowCount = Filas;
+            dgvMB.ColumnCount = Columnas;
+
+
+
+
+            
+
+
+            //MessageBox.Show("Datos De matriz B insertados");
+           
+
+        }
+
+        private void Sumar()
+        {
+
+            dgvResultado.Rows.Clear();
+            dgvResultado.Columns.Clear();
+
+            dgvResultado.RowCount = Filas;
+            dgvResultado.ColumnCount = Columnas;
+
+            for (i = 0; i < Filas; i++)
+            {
+                for (j = 0; j < Columnas; j++)
+                {
+                    MatrizA[i, j] = Convert.ToInt32(dgvMA.Rows[i].Cells[j].Value);
+                    MatrizB[i, j] = Convert.ToInt32(dgvMB.Rows[i].Cells[j].Value);
+
+                }
+            }
+
+            
+
+            
+
+
+            for (i = 0; i < Filas; i++)
+            {
+                for (j = 0; j < Columnas; j++)
+                {
+
+                    MatrizSuma[i, j] = MatrizA[i, j] + MatrizB[i, j];
+
+                    dgvResultado.Rows[i].Cells[j].Value = MatrizSuma[i, j];
+
+                }
+            }
+        }
+
+
+        private void Resar()
+        {
+
+            dgvResultado.Rows.Clear();
+            dgvResultado.Columns.Clear();
+
+            dgvResultado.RowCount = Filas;
+            dgvResultado.ColumnCount = Columnas;
+
+            for (i = 0; i < Filas; i++)
+            {
+                for (j = 0; j < Columnas; j++)
+                {
+                    MatrizA[i, j] = Convert.ToInt32(dgvMA.Rows[i].Cells[j].Value);
+                    MatrizB[i, j] = Convert.ToInt32(dgvMB.Rows[i].Cells[j].Value);
+
+                }
+            }
+
+
+
+
+
+
+            for (i = 0; i < Filas; i++)
+            {
+                for (j = 0; j < Columnas; j++)
+                {
+
+                    MatrizSuma[i, j] = MatrizA[i, j] - MatrizB[i, j];
+
+                    dgvResultado.Rows[i].Cells[j].Value = MatrizSuma[i, j];
+
+                }
+            }
+        }
+
+
+
+
+    }
+}
